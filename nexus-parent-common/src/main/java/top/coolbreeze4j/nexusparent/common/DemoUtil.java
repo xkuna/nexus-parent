e202bf52cc0a7c1f107f8c5cff19515fd5bdce16
xkuna <1614651354@qq.com>
2023-04-22 03:04:08 +0800

    提交聚合项目

---
+++ nexus-parent-common/src/main/java/top/coolbreeze4j/nexusparent/common/DemoUtil.java
@@ -0,0 +1,14 @@
+package top.coolbreeze4j.nexusparent.common;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author CoolBreeze
+ * @date 2023/4/21 17:49.
+ */
+@Slf4j
+public class DemoUtil {
+    public static void hello(){
+        log.info("hello nexus");
+    }
+}
---
+++ nexus-parent-service/src/main/java/top/coolbreeze4j/nexusparent/service/Main.java
@@ -0,0 +1,13 @@
+package top.coolbreeze4j.nexusparent.service;
+
+import top.coolbreeze4j.nexusparent.common.DemoUtil;
+
+/**
+ * @author CoolBreeze
+ * @date 2023/4/22 00:02.
+ */
+public class Main {
+    public static void main(String[] args) {
+        DemoUtil.hello();
+    }
+}
